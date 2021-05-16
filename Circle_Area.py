123c02943836da21937a1eef14f87423913123ae
Srijandas2000 <78365196+Srijandas2000@users.noreply.github.com>
2021-05-16 14:52:06 +0530

    Create Circle_Area.py

---
+++ Circle_Area.py
@@ -0,0 +1,6 @@
+# Circle Area Program by Srijan
+from math import pi
+print ("Calculate Area of a Cirle  ")
+r = float(input("Enter the radius of the Circle : "))
+area = pi*(r*r)
+print ("The Area of the circle with raius ",r," is : ", area)
