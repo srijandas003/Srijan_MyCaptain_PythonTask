8e2854796b43c2a3e8a75586188299cc9b0ee365
Srijandas2000 <78365196+Srijandas2000@users.noreply.github.com>
2021-05-16 14:48:44 +0530

    Create Find_Positive_in_List.py

---
+++ Find_Positive_in_List.py
@@ -0,0 +1,12 @@
+#Program to find pisitive numbers in a list by Srijan
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+print("list1 = " , list1)
+for num in list1:
+    if num>=0:
+        print(num,end = " ")
+
+print(print("\nlist2 = " , list2))
+for num in list2:
+    if num>=0:
+        print(num,end = " ")
